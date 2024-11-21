33902796c315de2adbc009d497d7442436f67acb
Dean Orion <60265116+Doer64@users.noreply.github.com>
2024-11-21 10:20:54 +0200

    Completed Algebra

--- Algebra.java
+++ Algebra.java
@@ -21,47 +21,104 @@ public class Algebra {
    		System.out.println(sqrt(36));
 		System.out.println(sqrt(263169));
    		System.out.println(sqrt(76123));
-	}  
+		} 
 
 	// Returns x1 + x2
 	public static int plus(int x1, int x2) {
-		// Replace the following statement with your code
-		return 0;
+		int sum = x1;
+		if(x2 < 0){
+			sum = minus(x1, -x2);
+		}
+		else{
+			for(int i = 0; i < x2; i++) {
+				sum++;
+			}
+		}
+		return sum;
 	}
 
 	// Returns x1 - x2
 	public static int minus(int x1, int x2) {
-		// Replace the following statement with your code
-		return 0;
+		int sum = x1;
+		if(x2 < 0){
+			sum = plus(x1, -x2);
+		}
+		else{
+			for(int i = 0; i < x2; i++) {
+				sum--;
+			}
+		}
+		return sum;
 	}
 
 	// Returns x1 * x2
 	public static int times(int x1, int x2) {
-		// Replace the following statement with your code
-		return 0;
+		int tSum = x1;
+		if(x1 == 0 || x2 == 0){
+			return 0;
+		} else if(x2 < 0){
+			for(int i = 1; i < x2; i++) {
+				tSum = plus(tSum, x1);
+			}
+			tSum = -tSum;
+		}
+		for(int i = 1; i < x2; i++) {
+			tSum = plus(tSum, x1);
+		}
+		return tSum;
 	}
 
 	// Returns x^n (for n >= 0)
 	public static int pow(int x, int n) {
-		// Replace the following statement with your code
-		return 0;
+		int tSum = x;
+		if(n == 0){
+			return 1;
+		}
+		else if (n < 0){	//if n is negative then x^n = 1/x^-n which is -1 < 1/x^-n < 1
+			return 0;
+		}
+		else {
+			for(int i = 1; i < n; i++) {
+				tSum = times(tSum, x);
+			}
+		}
+		return tSum;
 	}
 
 	// Returns the integer part of x1 / x2 
 	public static int div(int x1, int x2) {
-		// Replace the following statement with your code
-		return 0;
+		int divV = 0;	//div Value
+		if(x2 == 0){	//if x2 is 0 then x1/x2 is undefined
+			return 0;
+		}	
+		else if(x2 < 0){ //if x2 is negative then x1/x2 is negative
+			while (times(-x2, plus(divV, 1)) <= x1) {
+				divV++;
+			}
+			divV = -divV;
+		}
+		else while (times(x2, plus(divV, 1)) <= x1) {	//if x2 is positive then x1/x2 is positive
+			divV++;
+		}
+		return divV;
 	}
 
 	// Returns x1 % x2
 	public static int mod(int x1, int x2) {
-		// Replace the following statement with your code
-		return 0;
+		int divV = div(x1, x2);		//div Value
+		int modV = minus(x1, times(x2, divV));		//mod Value
+		return (x2 >= 0) ? modV : x2-modV;
 	}	
 
 	// Returns the integer part of sqrt(x) 
 	public static int sqrt(int x) {
-		// Replace the following statement with your code
-		return 0;
+		int count = 0;
+		if(x <= 0){
+			count = 0;
+		}
+		else while (pow(plus(count, 1) , 2) <= x) {
+			count++;
+		}
+		return count;
 	}	  	  
 }
